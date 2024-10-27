fe0fe824769f239f222ed96fd4a6312a7f3bc408
miss-gif <svx327@naver.com>
2024-10-27 18:33:34 +0900

    Refactor: Update project setup and language configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 import classNames from 'classnames/bind'
 import styles from './App.module.scss'
+import { useEffect, useState } from 'react'
+import FullScreenMessage from './components/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    const URL = 'http://localhost:8888/wedding'
+    fetch(URL)
+      .then((res) => {
+        if (res.ok === false) {
+          throw new Error('Network response was not ok')
+        }
+        return res.json()
+      })
+      .then((data) => {
+        setWedding(data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.log('에러발생', err)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+    return () => {}
+  }, [])
+
+  if (loading) {
+    return (
+      <>
+        <FullScreenMessage type="loading" />
+      </>
+    )
+  }
+
+  if (error) {
+    return (
+      <>
+        <FullScreenMessage type="error" />
+      </>
+    )
+  }
 
   return (
     <>
-      <div className={cx('container')}>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className={cx('container')}>{JSON.stringify(wedding)}</div>
     </>
   )
 }
