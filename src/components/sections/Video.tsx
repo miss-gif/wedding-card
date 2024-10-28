92c30fa3454ecd0caed7b7930868f38d18645d1a
miss-gif <svx327@naver.com>
2024-10-28 16:21:27 +0900

    Refactor: Update Video component to display a video with custom styling
    - Updated Video.tsx to include a video element with custom styling
    - Added Video.module.scss to define the styling for the video container
    - Added main.mp4 to the assets folder for the video source

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -5,7 +5,19 @@ import Section from '../shared/Section'
 const cx = className.bind(styles)
 
 const Video = () => {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.png"
+        // controls={true}
+      >
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  )
 }
 
 export default Video
